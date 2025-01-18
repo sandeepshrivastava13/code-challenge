f2d20bcebfee1ae3c9ef6e63e0bbb5038263b46e
sandeepshrivastava13 <74923683+sandeepshrivastava13@users.noreply.github.com>
2025-01-18 09:26:48 +0000

    Task 2

--- project/packages/ui/components/List.tsx
+++ project/packages/ui/components/List.tsx
@@ -1,5 +1,26 @@
-import React from 'react'
+import { useDispatch } from "react-redux"
+import { removePokemon } from "../../../apps/app/src/features/pokemon/pokemonSlice"
 
-export const List = () => {
-  return <p>List</p>
+
+interface Pokemon{
+  name:string,
+  url:string
+}
+
+interface ListProps {
+ list:Pokemon[]
+}
+
+
+export const List = (props:ListProps) => {  
+
+  const dispatch=useDispatch()
+  
+  return <div className="pokemon-list">
+    {props.list.map((item:Pokemon,index:number)=>
+    <div className="list-items" key={index}>
+     <h4>{item.name}</h4>
+     <button onClick={()=>dispatch(removePokemon(item.name))}>Delete</button>
+    </div>)}
+  </div>
 }
